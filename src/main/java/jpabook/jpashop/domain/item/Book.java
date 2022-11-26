fe5b582537646d086514a6abb1925f30e0359a0b
dschoi30 <ds0631@gmail.com>
2022-11-26 19:40:26 +0900

    ~엔티티 개발

---
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("B")
+@Getter @Setter
+public class Book extends Item {
+
+    private String author;
+    private String isbn;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
